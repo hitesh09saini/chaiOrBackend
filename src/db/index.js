ea6bd8fd38cc286b7ccf3c2b39931b3db173ced3
Hitesh Saini <hitesh9694saini@gmail.com>
2023-11-04 23:35:01 +0530

    push

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+const connectDB = async ()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGOOB__URI}/${DB_NAME}`);
+    console.log(`\n MongoDb is Connected !! DB Host `);
+  } catch (error) {
+    console.log("Error: "+error);
+    process.exit(1);
+  }
+}
+
+export default connectDB
+
