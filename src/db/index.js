51a2c9cc9e01b1086c2215b61fd46829efbac8b9
Hitesh Saini <hitesh9694saini@gmail.com>
2023-11-09 20:07:38 +0530

    push

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose"
 import {DB_NAME} from "../constants.js"
 
+
+
 const connectDB = async ()=>{
   try {
     await mongoose.connect(`${process.env.MONGOOB__URI}/${DB_NAME}`);
