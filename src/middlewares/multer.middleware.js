0227eb58468bd6461fd7944c37e7bc37dcc7f3b2
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-08 18:44:38 +0530

    add multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,14 +4,15 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
     }
-  })
-  
-  const upload = multer({ storage: storage })
+})
 
-  
+const upload = multer({ storage })
+
+export default upload
+
